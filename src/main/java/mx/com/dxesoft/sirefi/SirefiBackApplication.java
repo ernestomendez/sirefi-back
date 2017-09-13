5e9e4ca2fa86477693ee5add58e095088b9d9266
Ernesto Mendez <nernesto@gmail.com>
2017-09-12 23:14:10 -0700

    initial commit

---
+++ src/main/java/mx/com/dxesoft/sirefi/SirefiBackApplication.java
@@ -0,0 +1,12 @@
+package mx.com.dxesoft.sirefi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirefiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirefiBackApplication.class, args);
+	}
+}
